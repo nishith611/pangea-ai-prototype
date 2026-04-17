afde3aad6c7b3f7cb2749dba2f409b420c2cd126
nishith611 <nfr611@gmail.com>
2026-04-17 16:21:20 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PangeaAIPrototype from './pangea-ai-prototype'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PangeaAIPrototype />
+  </React.StrictMode>
+)
 
